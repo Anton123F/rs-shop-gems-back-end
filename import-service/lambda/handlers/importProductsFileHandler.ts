2a57b0e47f05cb96c6e64064ab22b2f1e3047a69
Anton Fiadotauu <anton_fiadotau@epam.com>
2024-07-09 01:10:30 +0300

    07_authorization

--- import-service/lambda/handlers/importProductsFileHandler.ts
+++ import-service/lambda/handlers/importProductsFileHandler.ts
@@ -7,7 +7,7 @@ const HEADERS =  {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Credentials': true,
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type'
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization'
 }
 
 export const handler = async (event: any) => {
